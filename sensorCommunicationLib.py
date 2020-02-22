5f5bd5d87a7a99f554a71716a68b66706c71130c
Kavya9294 <kavya.ub9294@gmail.com>
2020-02-22 15:53:11 -0500

    Skeleton for the sensor communication Library

---
+++ sensorCommunicationLib.py
@@ -0,0 +1,84 @@
+#Code for sensors communication
+
+
+
+#Code for reading from SparkFun IMU Breakout
+
+
+
+#Code for Writing to SparkFun IMU Breakout
+
+
+
+#Code for Reading from LM393
+
+
+
+#Code for Writing to LM393
+
+
+
+#Code for Reading from 19MM DIGITAL OUTPUT PRESSURE SENSOR
+
+
+
+#Code for Writing to 19MM DIGITAL OUTPUT PRESSURE SENSOR
+
+
+
+#Code for REading from PT100
+
+
+
+#Code for Writing to PT100
+
+
+
+#Code for Reading from MAX31865
+
+
+
+#Code for Writing to MAX31865
+
+
+
+#Code for Reading from Contact-less Infrared Thermopile Sensor
+
+
+
+#Code for Writing to Contact-less Infrared Thermopile Sensor
+
+
+
+#Code for Reading from UVX-300
+
+
+
+#Code to write to UVX-300
+
+
+
+#Code to REad from ELEGOO HC-SR04
+
+
+
+#Code to Write to ELEGOO HC-SR04
+
+
+
+#Code to read from 3202 Mini Spy Camera
+
+
+
+#Code to Write to 3202 Mini Spy Camera
+
+
+
+#Code to read from TRD-S2000VD
+
+
+
+#Code to Write to TRD-S2000VD
+
+
+
