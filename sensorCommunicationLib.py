52b1cb9b952fbea5a71bf09043183aee7129d48a
Demetri Lopez <demetri@10-18-107-243.dynapool.wireless.nyu.edu>
2020-03-07 15:42:12 -0500

    Update temp no contact sensor code python

--- sensorCommunicationLib.py
+++ sensorCommunicationLib.py
@@ -1,3 +1,8 @@
+import time
+import board
+import busio
+import adafruit_tmp006
+
 #Code for sensors communication
 
 
@@ -28,9 +33,23 @@
 
 #Code for Reading from TMP006 Contact-less Infrared Thermopile Sensor: Demitri
 
+def get_temp_ncontact():
+    # Define a function to convert celsius to fahrenheit.
+    def c_to_f(c):
+        return c * 9.0 / 5.0 + 32.0
 
+    # Create library object using Bus I2C port
+    i2c = busio.I2C(board.SCL, board.SDA)
+    sensor = adafruit_tmp006.TMP006(i2c)
 
-#Code for Writing to TMP006 Contact-less Infrared Thermopile Sensor: Demitri
+    # Initialize communication with the sensor, using the defaul16 samples
+    # This is the best accuracy but a little slower at reacting to changes.
+    # The first sample will be meaningless
+    while True:
+        obj_temp = sensor.temperature
+        print('Object temperature: {0:0.3F}*C / {1:0.3F}*F'.format(obj_temp, c_to_f(obj_temp)))
+        time.sleep(5.0)
+
 
 
 
